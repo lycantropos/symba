c2e8e303407e806efa74377d069a0dfaf6fe7cd4
Azat Ibrakov <azatibrakov@gmail.com>
2021-03-05 01:26:07 +0300

    Add test for 'Expression.__neg__' magic method

---
+++ tests/base_tests/expression_tests/test_neg.py
@@ -0,0 +1,18 @@
+from hypothesis import given
+
+from symba.base import Expression
+from . import strategies
+
+
+@given(strategies.expressions)
+def test_basic(expression: Expression) -> None:
+    result = -expression
+
+    assert isinstance(result, Expression)
+
+
+@given(strategies.expressions)
+def test_involution(expression: Expression) -> None:
+    result = -expression
+
+    assert -result == expression
