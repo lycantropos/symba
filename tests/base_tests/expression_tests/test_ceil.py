2765be0df267562a7f921d8be69fed56d2f26c60
Azat Ibrakov <azatibrakov@gmail.com>
2021-03-04 14:22:36 +0300

    Add test for 'Expression.__ceil__' magic method

---
+++ tests/base_tests/expression_tests/test_ceil.py
@@ -0,0 +1,21 @@
+import math
+
+from hypothesis import given
+
+from symba.base import Expression
+from . import strategies
+
+
+@given(strategies.expressions)
+def test_reflexivity(expression: Expression) -> None:
+    result = math.ceil(expression)
+
+    assert isinstance(result, int)
+
+
+@given(strategies.expressions)
+def test_value(expression: Expression) -> None:
+    result = math.ceil(expression)
+
+    assert result >= expression
+    assert result < expression + 1
