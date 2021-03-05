5654e4d5187130ef278287a8caa7f65c935df23d
Azat Ibrakov <azatibrakov@gmail.com>
2021-03-05 12:14:43 +0300

    Complete test for 'Expression.__mod__' magic method

--- tests/base_tests/expression_tests/test_mod.py
+++ tests/base_tests/expression_tests/test_mod.py
@@ -16,8 +16,29 @@ def test_basic(expression: Expression,
     assert isinstance(result, Expression)
 
 
+@given(strategies.expressions, strategies.non_zero_reals_or_expressions)
+def test_value(expression: Expression,
+               expression_or_real: Union[Real, Expression]) -> None:
+    result = expression % expression_or_real
+
+    assert (result == 0 and (expression / expression_or_real
+                             == expression // expression_or_real)
+            or (result > 0) is (expression_or_real > 0))
+    assert abs(result) < abs(expression_or_real)
+
+
 @given(strategies.expressions)
 def test_modulo_one(expression: Expression) -> None:
     result = expression % 1
 
     assert result == expression - math.floor(expression)
+
+
+@given(strategies.expressions, strategies.non_zero_reals_or_expressions)
+def test_connection_with_floordiv(expression: Expression,
+                                  expression_or_real: Union[Real, Expression]
+                                  ) -> None:
+    result = expression % expression_or_real
+
+    assert (result + (expression // expression_or_real) * expression_or_real
+            == expression)
