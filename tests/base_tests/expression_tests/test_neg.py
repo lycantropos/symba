329c82ccef60594918ab943d9f8a196b879ebc99
Azat Ibrakov <azatibrakov@gmail.com>
2021-03-05 15:23:33 +0300

    Complete test for 'Expression.__neg__' magic method

--- tests/base_tests/expression_tests/test_neg.py
+++ tests/base_tests/expression_tests/test_neg.py
@@ -1,3 +1,6 @@
+from numbers import Real
+from typing import Union
+
 from hypothesis import given
 
 from symba.base import Expression
@@ -16,3 +19,46 @@ def test_involution(expression: Expression) -> None:
     result = -expression
 
     assert -result == expression
+
+
+@given(strategies.expressions, strategies.reals_or_expressions)
+def test_add_operand(expression: Expression,
+                     real_or_expression: Expression) -> None:
+    result = -(expression + real_or_expression)
+
+    assert result == (-expression) + (-real_or_expression)
+
+
+@given(strategies.expressions, strategies.expressions)
+def test_sub_operand(expression: Expression,
+                     real_or_expression: Union[Real, Expression]) -> None:
+    result = -(expression - real_or_expression)
+
+    assert result == (-expression) - (-real_or_expression)
+
+
+@given(strategies.expressions, strategies.expressions)
+def test_mul_operand(expression: Expression,
+                     real_or_expression: Expression) -> None:
+    result = -(expression * real_or_expression)
+
+    assert (result == (-expression) * real_or_expression
+            == expression * (-real_or_expression))
+
+
+@given(strategies.expressions, strategies.non_zero_reals_or_expressions)
+def test_truediv_operand(expression: Expression,
+                         real_or_expression: Expression) -> None:
+    result = -(expression / real_or_expression)
+
+    assert (result == (-expression) / real_or_expression
+            == expression / (-real_or_expression))
+
+
+@given(strategies.reals_or_expressions, strategies.non_zero_expressions)
+def test_rtruediv_operand(real_or_expression: Union[Real, Expression],
+                          expression: Expression) -> None:
+    result = -(real_or_expression / expression)
+
+    assert (result == (-real_or_expression) / expression
+            == real_or_expression / (-expression))
