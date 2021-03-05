e8583e9f49aed41db1b61f2df59e30c30a321688
Azat Ibrakov <azatibrakov@gmail.com>
2021-03-05 05:52:12 +0300

    Add test for 'Expression.__hash__' magic method

---
+++ tests/base_tests/expression_tests/test_hash.py
@@ -0,0 +1,26 @@
+from hypothesis import given
+
+from symba.base import Expression
+from tests.utils import equivalence
+from . import strategies
+
+
+@given(strategies.expressions)
+def test_basic(expression: Expression) -> None:
+    result = hash(expression)
+
+    assert isinstance(result, int)
+
+
+@given(strategies.expressions)
+def test_determinism(expression: Expression) -> None:
+    result = hash(expression)
+
+    assert result == hash(expression)
+
+
+@given(strategies.expressions, strategies.expressions)
+def test_connection_with_equality(left_expression: Expression,
+                                  right_expression: Expression) -> None:
+    assert equivalence(left_expression == right_expression,
+                       hash(left_expression) == hash(right_expression))
