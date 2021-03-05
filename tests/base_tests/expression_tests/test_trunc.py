74900fc35041eededbd1fd9e3194dfa635f45d72
Azat Ibrakov <azatibrakov@gmail.com>
2021-03-05 06:49:27 +0300

    Add test for 'Expression.__trunc__' magic method

---
+++ tests/base_tests/expression_tests/test_trunc.py
@@ -0,0 +1,20 @@
+import math
+
+from hypothesis import given
+
+from symba.base import Expression
+from . import strategies
+
+
+@given(strategies.expressions)
+def test_basic(expression: Expression) -> None:
+    result = math.trunc(expression)
+
+    assert isinstance(result, int)
+
+
+@given(strategies.expressions)
+def test_value(expression: Expression) -> None:
+    result = math.trunc(expression)
+
+    assert abs(result - expression) < 1
