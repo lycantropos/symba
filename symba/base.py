c7b1e79ebacb5576c732051ff785a46d62439077
Azat Ibrakov <azatibrakov@gmail.com>
2021-03-15 14:56:08 +0300

    Add docstrings for 'base' module

--- symba/base.py
+++ symba/base.py
@@ -12,14 +12,34 @@ Expression = Expression
 
 
 def get_sqrt_evaluator() -> _SqrtEvaluator:
+    """
+    Returns current square root evaluator.
+    """
     return _context.sqrt_evaluator.get()
 
 
 def set_sqrt_evaluator(evaluator: _SqrtEvaluator) -> None:
+    """
+    Sets square root evaluator.
+    """
     _context.sqrt_evaluator.set(evaluator)
 
 
 def sqrt(argument: _Union[_Real, Expression]) -> Expression:
+    """
+    Returns square root of the argument:
+        exact if it is a perfect square, symbolic instead.
+
+    >>> sqrt(0) == 0
+    True
+    >>> sqrt(1) == 1
+    True
+    >>> square_root_of_two = sqrt(2)
+    >>> square_root_of_two ** 2 == 2
+    True
+    >>> 1 < square_root_of_two < 2
+    True
+    """
     if argument < 0:
         raise ValueError('Argument should be non-negative.')
     return _Term.from_components(_One, _to_constant(argument))
