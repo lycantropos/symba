87a6c218789ff786da504d9fb148e303796d4e25
Azat Ibrakov <azatibrakov@gmail.com>
2021-04-20 21:59:59 +0300

    Complete test for 'sqrt' function

--- tests/base_tests/test_sqrt.py
+++ tests/base_tests/test_sqrt.py
@@ -35,6 +35,13 @@ def test_value(value: Union[Real, Expression]) -> None:
             or result == value == math.inf)
 
 
+@given(strategies.definite_non_negative_reals_or_expressions)
+def test_round_trip(value: Union[Real, Expression]) -> None:
+    result = sqrt(value)
+
+    assert result ** 2 == value
+
+
 @given(strategies.definite_negative_reals_or_expressions)
 def test_negative_argument(value: Union[Real, Expression]) -> None:
     with pytest.raises(ValueError):
