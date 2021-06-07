78938f91612ac8948251a88e3a855bc7c3f56951
Azat Ibrakov <azatibrakov@gmail.com>
2021-06-07 13:24:55 +0300

    Complete methods array

--- src/symba.c
+++ src/symba.c
@@ -6,7 +6,7 @@ static long to_square_free_long(long value) {
   for (long factor_candidate = 3, factor_candidate_squared = 9;
        factor_candidate_squared < value;
        factor_candidate_squared += 2 * factor_candidate + 1,
-           factor_candidate += 2) {
+            factor_candidate += 2) {
     while (!(value % factor_candidate_squared))
       value /= factor_candidate_squared;
   }
@@ -18,32 +18,31 @@ static long long to_square_free_long_long(long long value) {
   for (long long factor_candidate = 3, factor_candidate_squared = 9;
        factor_candidate_squared < value;
        factor_candidate_squared += 2 * factor_candidate + 1,
-           factor_candidate += 2) {
+                 factor_candidate += 2) {
     while (!(value % factor_candidate_squared))
       value /= factor_candidate_squared;
   }
   return value;
 }
 
-static PyObject *to_square_free(PyObject *self, PyObject *integer)
-{
-    int overflow_flag;
-    long value = PyLong_AsLongAndOverflow(integer, &overflow_flag);
-    if (value == -1) {
-      if (PyErr_Occurred())
-        return NULL;
-      else if (overflow_flag) {
-        long long value = PyLong_AsLongLong(integer);
-        if (value == -1 && PyErr_Occurred())
-          return NULL;
-        return PyLong_FromLongLong(to_square_free_long_long(value));
-      }
+static PyObject* to_square_free(PyObject* self, PyObject* integer) {
+  int overflow_flag;
+  long value = PyLong_AsLongAndOverflow(integer, &overflow_flag);
+  if (value == -1) {
+    if (PyErr_Occurred())
+      return NULL;
+    else if (overflow_flag) {
+      long long value = PyLong_AsLongLong(integer);
+      if (value == -1 && PyErr_Occurred()) return NULL;
+      return PyLong_FromLongLong(to_square_free_long_long(value));
     }
-    return PyLong_FromLong(to_square_free_long(value));
+  }
+  return PyLong_FromLong(to_square_free_long(value));
 }
 
 static PyMethodDef _symba_methods[] = {
     {"to_square_free", (PyCFunction)to_square_free, METH_O, NULL},
+    {NULL, NULL, 1, NULL} /* sentinel */
 };
 
 static PyModuleDef _symba_module = {
@@ -54,6 +53,4 @@ static PyModuleDef _symba_module = {
     .m_size = -1,
 };
 
-PyMODINIT_FUNC PyInit__symba(void) {
-  return PyModule_Create(&_symba_module);
-}
+PyMODINIT_FUNC PyInit__symba(void) { return PyModule_Create(&_symba_module); }
