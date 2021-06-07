65eb6a2aef35cee6095060e2444587ecf60c16c9
Azat Ibrakov <azatibrakov@gmail.com>
2021-06-07 15:40:08 +0300

    Complete formatting

--- src/symba.c
+++ src/symba.c
@@ -4,9 +4,8 @@
 static long to_square_free_long(long value) {
   while (!(value % 4)) value /= 4;
   for (long factor_candidate = 1, factor_candidate_squared = 9;
-       factor_candidate_squared < value;
-       factor_candidate += 2,
-       factor_candidate_squared += 4 * factor_candidate + 4) {
+       factor_candidate_squared < value; factor_candidate += 2,
+            factor_candidate_squared += 4 * factor_candidate + 4) {
     while (!(value % factor_candidate_squared))
       value /= factor_candidate_squared;
   }
@@ -16,9 +15,8 @@ static long to_square_free_long(long value) {
 static long long to_square_free_long_long(long long value) {
   while (!(value % 4)) value /= 4;
   for (long long factor_candidate = 1, factor_candidate_squared = 9;
-       factor_candidate_squared < value;
-       factor_candidate += 2,
-       factor_candidate_squared += 4 * factor_candidate + 4) {
+       factor_candidate_squared < value; factor_candidate += 2,
+                 factor_candidate_squared += 4 * factor_candidate + 4) {
     while (!(value % factor_candidate_squared))
       value /= factor_candidate_squared;
   }
