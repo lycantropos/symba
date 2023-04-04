b20d0fddfd5066e64b9dc610d5fffe37b8282e4e
Azat Ibrakov <azatibrakov@gmail.com>
2023-04-05 01:04:13 +0200

    Complete metadata

--- setup.py
+++ setup.py
@@ -1,17 +1,9 @@
 import platform
-from pathlib import Path
 
 from setuptools import (find_packages,
                         setup)
 
 project_base_url = 'https://github.com/lycantropos/symba/'
-
-
-def read_file(path_string: str) -> str:
-    return Path(path_string).read_text(encoding='utf-8')
-
-
-package_data = {'symba': ['py.typed']}
 parameters = dict(packages=find_packages(exclude=('tests', 'tests.*')),
                   url=project_base_url,
                   download_url=project_base_url + 'archive/master.zip')
@@ -20,8 +12,7 @@ if platform.python_implementation() == 'CPython':
 
     from setuptools import Extension
 
-    package_data['symba'].append('_symba.pyi')
-    parameters.update(ext_modules=[Extension('symba._symba', glob('src/*.c'))],
+    parameters.update(package_data={'symba': ['_symba.pyi']},
+                      ext_modules=[Extension('symba._symba', glob('src/*.c'))],
                       zip_safe=False)
-parameters['package_data'] = package_data
 setup(**parameters)
