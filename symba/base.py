1fe22df3d7a1fd2cf180587aa431e4bbf85919d8
Azat Ibrakov <azatibrakov@gmail.com>
2023-05-12 00:57:41 +0200

    Fix 'sqrt' function

--- symba/base.py
+++ symba/base.py
@@ -3,7 +3,7 @@ from typing import Union as _Union
 from .core import expression as _expression
 from .core.constant import (ONE as _ONE,
                             Infinite as _Infinite,
-                            to_constant as _to_constant)
+                            try_to_constant as _try_to_constant)
 from .core.hints import RawConstant as _RawConstant
 from .core.term import Term as _Term
 
@@ -29,7 +29,10 @@ def sqrt(argument: _Union[_RawConstant, Expression]) -> Expression:
         raise ValueError('Argument should be non-negative.')
     expression = (argument
                   if isinstance(argument, Expression)
-                  else _to_constant(argument))
+                  else _try_to_constant(argument))
+    if not isinstance(expression, Expression):
+        raise TypeError('Argument is not convertible to expression: '
+                        f'{argument}.')
     return (expression
             if isinstance(expression, _Infinite)
             else _Term.from_components(_ONE, expression))
